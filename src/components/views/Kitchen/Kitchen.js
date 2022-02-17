27692c1fd98d72d031f7a4fbfcde69a855eb1393
Orzechowa <orzechowska1989@wp.pl>
2022-02-17 22:52:11 +0100

    add 22.1

--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -1,11 +1,70 @@
 import styles from './Kitchen.module.scss';
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Checkbox, FormControlLabel } from '@material-ui/core';
+
+const orderContent = [
+  {id: '1', table: 1, status: 'ready', order: 987},
+  {id: '2', table: 2, status: 'not ready', order: null},
+  {id: '3', table: 3, status: 'ready', order: 654},
+];
+
+const renderActions = status => {
+  switch (status) {
+    case 'ready':
+      return (
+        <FormControlLabel control={<Checkbox checked={true} name="checkR" />} label="ready" />
+      );
+    case 'not ready':
+      return (
+        <FormControlLabel control={<Checkbox checked={false} name="checkNR" />} label="not ready" />
+      );
+    default:
+      return null;
+  }
+};
 
 const Kitchen = () => (
-  <div className = {styles.component}>
-    <h2> Kitchen View </h2>
-  </div>
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Id</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Order number</TableCell>
+          <TableCell>Details</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {orderContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {row.order}
+            </TableCell>
+            <TableCell>
+              {row.details}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
 );
+
 
 export default Kitchen;
 
--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,9 +1,35 @@
 import styles from './Login.module.scss';
 import React from 'react';
+import { Paper, Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
 
 const Login = () => (
   <div className = {styles.component}>
-    <h2> Login View </h2>
+    <Paper>
+      <Typography className={styles.title} variant="h4" component="h4" gutterBottom>
+        Sign in
+      </Typography>
+      <form className={styles.wrapper}>
+        <TextField className={styles.input}
+          id="filled-password-input"
+          label="Login"
+          type="text"
+          autoComplete="current-password"
+          variant="filled"
+        />
+        <TextField className={styles.input}
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+        />
+        <Button className={styles.button} variant="contained" color="primary" href={process.env.PUBLIC_URL}>
+          Log in
+        </Button>
+      </form>
+    </Paper>
   </div>
 );
 
